e74e2c9fc0c5f776dcb9eff3573866367df841d8
jolson615 <jolson615@gmail.com>
2018-07-08 17:35:23 -0400

    initial commit

---
+++ Basic Functions.playground/Contents.swift
@@ -0,0 +1,61 @@
+//: Playground - noun: a place where people can play
+
+// Level 1:
+// Here's a function that takes in a number and returns a string telling whether the number is positive or negative.
+func isItPositive(x: Int) -> String {
+    if x > 0 {
+        return "\(x) is a positive number."
+    } else if x < 0 {
+        return "\(x) is a negative number."
+    } else {
+        return "That number isn't positive or negative! It's \(x)!"
+    }
+}
+
+// Now try to call that function at least four different times. Try it with two different positive numbers, two different negative numbers.
+
+
+
+
+// Math bonus: Try to get the third statement to come out by passing in a number that is neither positive nor negative.
+
+
+
+// Level 2:
+// Create your own function that will tell you whether a number is divisible by three. Hint: You'll probably want to use a % operator at some point to make your life easier.
+
+
+
+// Call the function at least three different times - at least once with an argument that is a multiple of three, and at least once with a number that is not, to confirm that it works in both cases.
+
+
+
+// Now create a function that tells whether a number is even or odd.
+
+
+
+// Call that function three times to confirm that it works.
+
+
+
+
+// Level 3:
+// Create your own function that takes in someone's name as a string and returns another string telling whether or not that user's name is short (under 5 letters), medium (between 5-9 letters), or long (10 letters or more).
+
+
+
+// Call that function three times with three different name lengths to check if it works.
+
+
+
+// Create your own function that prints an invitation to a potluck. It will take in a person's name as a string, and a specific dish to bring as a string, and returns a long string that politely invites them to the potluck.
+// For example, if I called inviteToPotluck(name: "Jo", dish: "chips"), it would return a string like "Dear Jo, please join us tomorrow for a potluck. We'd love it if you could bring some chips!"
+
+
+
+// Call that function with three different people and food item assignments to confirm that it works.
+
+
+
+
+
